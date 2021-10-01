e7f54f0dd21ed7bfc9c3a318cdbdf04d83ba254b
Philip Brohan <philip@brohan.org>
2021-10-01 14:41:40 +0100

    Added a VAE working on sequences of pressure fields.

---
+++ models/DCVAE_sequence_PRMSL/autoencoder.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+# Convolutional Variational Autoencoder for 20CR2c MSLP
+
+# This one fits a sequence of 5 6-hourly fields
+
+import os
+import sys
+import time
+import tensorflow as tf
+import pickle
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--epoch", help="Restart from epoch", type=int, required=False, default=0
+)
+args = parser.parse_args()
+
+# Distribute across all GPUs
+# strategy = tf.distribute.MirroredStrategy()
+# Doesn't yet work - need to change the training loop and datasets to be
+#  strategy aware.
+strategy = tf.distribute.get_strategy()
+
+# Load the model specification
+sys.path.append("%s/." % os.path.dirname(__file__))
+from autoencoderModel import DCVAE
+from autoencoderModel import train_step
+from autoencoderModel import compute_loss
+
+# Load the data source provider
+sys.path.append("%s/../PRMSL_dataset" % os.path.dirname(__file__))
+from makeSequenceDataset import getDataset
+
+# How big is the latent space?
+latent_dim = 100
+
+# How many images to use?
+nTrainingImages = 5989  # Max is 5989
+nTestImages = 665  # Max is 665
+
+# How many epochs to train for
+nEpochs = 500
+# Length of an epoch - if None, same as nTrainingImages
+nImagesInEpoch = None
+
+if nImagesInEpoch is None:
+    nImagesInEpoch = nTrainingImages
+
+# Dataset parameters
+bufferSize = 1000  # Untested
+batchSize = 32  # Arbitrary
+
+# Set up the training data
+trainingData = getDataset(purpose="training", nImages=nTrainingImages).repeat(5)
+trainingData = trainingData.shuffle(bufferSize).batch(batchSize)
+
+# Set up the test data
+testData = getDataset(purpose="test", nImages=nTestImages)
+testData = testData.batch(batchSize)
+
+# Instantiate the model
+with strategy.scope():
+    autoencoder = DCVAE(latent_dim)
+    optimizer = tf.keras.optimizers.Adam(1e-4)
+    # If we are doing a restart, load the weights
+    if args.epoch > 0:
+        weights_dir = ("%s/Proxy_20CR/models/DCVAE_sequence_PRMSL/" + "Epoch_%04d") % (
+            os.getenv("SCRATCH"),
+            args.epoch - 1,
+        )
+        load_status = autoencoder.load_weights("%s/ckpt" % weights_dir)
+        # Check the load worked
+        load_status.assert_existing_objects_matched()
+
+
+# Save the model weights and the history state after every epoch
+history = {}
+history["loss"] = []
+history["val_loss"] = []
+
+
+def save_state(model, epoch, loss):
+    save_dir = ("%s/Proxy_20CR/models/DCVAE_sequence_PRMSL/" + "Epoch_%04d") % (
+        os.getenv("SCRATCH"),
+        epoch,
+    )
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+    model.save_weights("%s/ckpt" % save_dir)
+    history["loss"].append(loss)
+    # history["val_loss"].append(logs["val_loss"])
+    history_file = "%s/history.pkl" % save_dir
+    pickle.dump(history, open(history_file, "wb"))
+
+
+for epoch in range(nEpochs):
+    start_time = time.time()
+    for train_x in trainingData:
+        train_step(autoencoder, train_x, optimizer)
+    end_time = time.time()
+
+    train_rmse = tf.keras.metrics.Mean()
+    train_logpz = tf.keras.metrics.Mean()
+    train_logqz_x = tf.keras.metrics.Mean()
+    for test_x in trainingData:
+        (rmse, logpz, logqz_x) = compute_loss(autoencoder, test_x)
+        train_rmse(rmse)
+        train_logpz(logpz)
+        train_logqz_x(logqz_x)
+    test_rmse = tf.keras.metrics.Mean()
+    test_logpz = tf.keras.metrics.Mean()
+    test_logqz_x = tf.keras.metrics.Mean()
+    for test_x in trainingData:
+        (rmse, logpz, logqz_x) = compute_loss(autoencoder, test_x)
+        test_rmse(rmse)
+        test_logpz(logpz)
+        test_logqz_x(logqz_x)
+    print("Epoch: {}".format(epoch))
+    print("RMSE: {}, {}".format(train_rmse.result(), test_rmse.result()))
+    print("logpz: {}, {}".format(train_logpz.result(), test_logpz.result()))
+    print("logqz_x: {}, {}".format(train_logqz_x.result(), test_logqz_x.result()))
+    print("time: {}".format(end_time - start_time))
+    save_state(autoencoder, epoch, test_rmse.result())
---
+++ models/DCVAE_sequence_PRMSL/autoencoderModel.py
@@ -0,0 +1,164 @@
+# Specify a Deep Convolutional Variational AutoEncoder
+#  for the 20CR2c PRMSL fields
+
+# Based on the TF tutorial at:
+#   https://www.tensorflow.org/tutorials/generative/cvae
+
+# This version fits a sequence of 5 6-hourly fields.
+
+import tensorflow as tf
+import numpy as np
+import sys
+
+
+class DCVAE(tf.keras.Model):
+    def __init__(self, latent_dim):
+        super(DCVAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.encoder = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(80, 160, 5)),
+                # tf.keras.layers.Dropout(0.2),
+                tf.keras.layers.Conv2D(
+                    filters=5,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="same",
+                    activation="relu",
+                ),
+                # tf.keras.layers.SpatialDropout2D(0.2),
+                tf.keras.layers.Conv2D(
+                    filters=5,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="same",
+                    activation="relu",
+                ),
+                # tf.keras.layers.SpatialDropout2D(0.2),
+                tf.keras.layers.Conv2D(
+                    filters=10,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="same",
+                    activation="relu",
+                ),
+                # tf.keras.layers.SpatialDropout2D(0.2),
+                tf.keras.layers.Conv2D(
+                    filters=20,
+                    kernel_size=3,
+                    strides=(2, 2),
+                    padding="same",
+                    activation="relu",
+                ),
+                # tf.keras.layers.SpatialDropout2D(0.2),
+                tf.keras.layers.Flatten(),
+                # No activation
+                tf.keras.layers.Dense(latent_dim + latent_dim),
+                # tf.keras.layers.BatchNormalization(),
+                # tf.keras.layers.GaussianNoise(2.0),
+            ]
+        )
+
+        self.decoder = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+                # tf.keras.layers.InputLayer(input_shape=(5*10*20)),
+                tf.keras.layers.Dense(units=5 * 10 * 20, activation=tf.nn.relu),
+                tf.keras.layers.Reshape(target_shape=(5, 10, 20)),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=10,
+                    kernel_size=3,
+                    strides=2,
+                    padding="same",
+                    activation="relu",
+                ),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=5,
+                    kernel_size=3,
+                    strides=2,
+                    padding="same",
+                    activation="relu",
+                ),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=5,
+                    kernel_size=3,
+                    strides=2,
+                    padding="same",
+                    activation="relu",
+                ),
+                # No activation
+                tf.keras.layers.Conv2DTranspose(
+                    filters=5, kernel_size=3, strides=2, padding="same"
+                ),
+            ]
+        )
+
+    def encode(self, x):
+        mean, logvar = tf.split(self.encoder(x), num_or_size_splits=2, axis=1)
+        return mean, logvar
+
+    def decode(self, z):
+        decoded = self.decoder(z)
+        return decoded
+
+    def call(self, x):
+        mean, logvar = self.encode(x)
+        latent = self.reparameterize(mean, logvar)
+        encoded = self.decode(latent)
+        return encoded
+
+    def reparameterize(self, mean, logvar):
+        eps = tf.random.normal(shape=mean.shape)
+        return eps * tf.exp(logvar * 0.5) + mean
+
+    # Decode each member of the batch several times, to make a sample
+    #  returns a 4d tensor (size, batch, y, x)
+    def sample_decode(self, mean, logvar, size=100):
+        encoded = []
+        eps = tf.random.normal(shape=(size, self.latent_dim))
+        mean = tf.unstack(mean, axis=0)
+        logvar = tf.unstack(logvar, axis=0)
+        for batchI in range(len(mean)):
+            latent = eps * tf.exp(logvar[batchI] * 0.5) + mean[batchI]
+            encoded.append(self.decode(latent))
+        return tf.stack(encoded, axis=0)
+
+    # Autoencode each member of the batch several times, to make a sample
+    def sample_call(self, x, size=100):
+        mean, logvar = self.encode(x)
+        encoded = self.sample_decode(mean, logvar, size=size)
+        return encoded
+
+
+def log_normal_pdf(sample, mean, logvar, raxis=1):
+    log2pi = tf.math.log(2.0 * np.pi)
+    return tf.reduce_sum(
+        -0.5 * ((sample - mean) ** 2.0 * tf.exp(-logvar) + logvar + log2pi), axis=raxis
+    )
+
+
+def compute_loss(model, x):
+    mean, logvar = model.encode(x)
+    latent = model.reparameterize(mean, logvar)
+    encoded = model.decode(latent)
+    rmse = tf.reduce_mean(tf.keras.metrics.mean_squared_error(encoded, x), axis=[1, 2])
+    # print(rmse)
+    logpz = log_normal_pdf(latent, 0.0, 0.0)
+    # print(logpz)
+    # sys.exit(0)
+    logqz_x = log_normal_pdf(latent, mean, logvar)
+    return (rmse * 10000, logpz, logqz_x)
+
+
+@tf.function  # Optimiser ~25% speedup on VDI (CPU-only)
+def train_step(model, x, optimizer):
+    """Executes one training step and returns the loss.
+
+    This function computes the loss and gradients, and uses the latter to
+    update the model's parameters.
+    """
+    with tf.GradientTape() as tape:
+        (rmse, logpz, logqz_x) = compute_loss(model, x)
+        metric = tf.reduce_mean(rmse - logpz + logqz_x)
+    gradients = tape.gradient(metric, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
