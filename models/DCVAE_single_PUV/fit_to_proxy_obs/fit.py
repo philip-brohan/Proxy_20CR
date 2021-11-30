9fec30522ded55f22d7994f340c1b29d9ef7a5db
Philip Brohan <philip@brohan.org>
2021-11-30 17:24:53 +0000

    Added fit-to-observations script for PUV.

--- models/DCVAE_single_PUV/fit_to_proxy_obs/fit.py
+++ models/DCVAE_single_PUV/fit_to_proxy_obs/fit.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
-# Find a point in latent space that maximises the fit to a test case,
+# Find a point in latent space that maximises the fit to a set of pseudo-obs,
 #  and plot the fitted state.
 
+import os
+import sys
 
 import tensorflow as tf
 import tensorflow_probability as tfp
-import os
-import sys
+from tensorflow_addons.image import interpolate_bilinear
+
 import random
+
+import iris
+import IRData.twcr as twcr
+import datetime
 
 import matplotlib
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -18,7 +24,13 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--epoch", help="Epoch", type=int, required=True)
-parser.add_argument("--case", help="Test case", type=int, required=False, default=0)
+parser.add_argument("--case", help="Epoch", type=int, required=False, default=0)
+parser.add_argument("--year", help="Year", type=int, required=True)
+parser.add_argument("--month", help="Integer month", type=int, required=True)
+parser.add_argument("--day", help="Day of month", type=int, required=True)
+parser.add_argument(
+    "--hour", help="Time of day (0, 6, 12, or 18)", type=int, required=True
+)
 parser.add_argument("--mslp", dest="mslp", default=False, action="store_true")
 parser.add_argument("--uwnd", dest="uwnd", default=False, action="store_true")
 parser.add_argument("--vwnd", dest="vwnd", default=False, action="store_true")
@@ -27,6 +39,19 @@ args = parser.parse_args()
 sys.path.append("%s/../validation" % os.path.dirname(__file__))
 from plot_prmsl_comparison import get_land_mask
 from plot_prmsl_comparison import plot_PRMSL
+
+# Get the ob locations at the given time from 2c
+dte = datetime.datetime(args.year, args.month, args.day, args.hour)
+obs = twcr.load_observations_1file(dte, version="2c")
+# Convert the obs locations to a tensor in the right units (0-1)
+t_lats = (obs["Latitude"].values + 90) / 180
+t_lons = (obs["Longitude"].values) / 360
+t_lons[t_lons > 0.5] -= 1
+t_lons += 0.5
+t_lats = tf.convert_to_tensor(t_lats, tf.float32)
+t_lons = tf.convert_to_tensor(t_lons, tf.float32)
+t_obs = tf.stack((t_lats * 80, t_lons * 160), axis=1)
+t_obs = tf.expand_dims(t_obs, 0)
 
 # Load the data source provider
 sys.path.append("%s/../../PUV_dataset" % os.path.dirname(__file__))
@@ -46,47 +71,65 @@ weights_dir = ("%s/Proxy_20CR/models/DCVAE_single_PUV/" + "Epoch_%04d") % (
 load_status = autoencoder.load_weights("%s/ckpt" % weights_dir)
 # Check the load worked
 load_status.assert_existing_objects_matched()
-# We are using it in inference mode - (does this have any effect?)
+# We are using it in inference mode
+# (I'm not at all sure this actually works)
 autoencoder.decoder.trainable = False
 for layer in autoencoder.decoder.layers:
     layer.trainable = False
 autoencoder.decoder.compile()
-
 
 count = 0
 for t_in in testData:
     if count == args.case:
         latent = tf.Variable(tf.random.normal(shape=(1, autoencoder.latent_dim)))
         target = tf.constant(tf.reshape(t_in, [1, 80, 160, 3]))
-        # print(latent)
+        # print(t_obs.shape)
+        # sys.exit(0)
+
+        exact = tf.squeeze(interpolate_bilinear(target, t_obs, indexing="ij"))
+        # print(exact[0:1000])
+        # Filter out the nans (bad lat/lon)
+        msk = tf.reduce_mean(exact, axis=1)
+        t_obs = tf.boolean_mask(t_obs, ~tf.math.is_nan(msk), axis=1)
+        exact = tf.boolean_mask(exact, ~tf.math.is_nan(msk), axis=0)
+
+        # Split the obs by variable
+        o_mslp = exact[:, 0]
+        o_mslp = o_mslp + tf.random.normal(
+            shape=o_mslp.shape, mean=0.0, stddev=2.0 / 30, dtype=tf.float32
+        )
+        o_uwnd = exact[:, 1]
+        o_uwnd = o_uwnd + tf.random.normal(
+            shape=o_uwnd.shape, mean=0.0, stddev=2.0 / 30, dtype=tf.float32
+        )
+        o_vwnd = exact[:, 2]
+        o_vwnd = o_vwnd + tf.random.normal(
+            shape=o_vwnd.shape, mean=0.0, stddev=2.0 / 30, dtype=tf.float32
+        )
+
         def decodeFit():
-            result = 0.0
             decoded = autoencoder.decode(latent)
+            at_obs = tf.squeeze(interpolate_bilinear(decoded, t_obs, indexing="ij"))
+            result = 0.0
             if args.mslp:
                 result = result + tf.reduce_mean(
-                    tf.keras.metrics.mean_squared_error(
-                        decoded[:, :, :, 0], target[:, :, :, 0]
-                    )
+                    tf.keras.metrics.mean_squared_error(o_mslp, at_obs[:, 0])
                 )
             if args.uwnd:
                 result = (
                     result
                     + tf.reduce_mean(
-                        tf.keras.metrics.mean_squared_error(
-                            decoded[:, :, :, 1], target[:, :, :, 1]
-                        )
+                        tf.keras.metrics.mean_squared_error(o_uwnd, at_obs[:, 1])
                     )
-                    / 10
+                    / 1
                 )
             if args.vwnd:
                 result = (
                     result
                     + tf.reduce_mean(
-                        tf.keras.metrics.mean_squared_error(
-                            decoded[:, :, :, 2], target[:, :, :, 2]
-                        )
+                        tf.keras.metrics.mean_squared_error(o_vwnd, at_obs[:, 2])
                     )
-                    / 10
+                    / 1
                 )
             return result
 
@@ -99,6 +142,7 @@ for t_in in testData:
         print(loss)
         break
         # sys.exit(0)
+    count += 1
 
 # latent = tf.Variable(tf.random.normal(shape=(1, autoencoder.latent_dim)))
 # load_status = autoencoder.load_weights("%s/ckpt" % weights_dir)
@@ -128,6 +172,8 @@ plot_PRMSL(
     tf.reshape(target[:, :, :, 2], [80, 160]),
     tf.reshape(encoded[:, :, :, 2], [80, 160]),
     c_space=0.2,
+    linewidths=[2, 2, 1],
+    obs=t_obs,
     land=lm,
     label="vwnd",
 )
@@ -139,6 +185,8 @@ plot_PRMSL(
     tf.reshape(target[:, :, :, 1], [80, 160]),
     tf.reshape(encoded[:, :, :, 1], [80, 160]),
     c_space=0.2,
+    linewidths=[2, 2, 1],
+    obs=t_obs,
     land=lm,
     label="uwnd",
 )
@@ -149,6 +197,9 @@ plot_PRMSL(
     ax_plot,
     tf.reshape(target[:, :, :, 0], [80, 160]),
     tf.reshape(encoded[:, :, :, 0], [80, 160]),
+    c_space=0.1,
+    linewidths=[2, 2, 1],
+    obs=t_obs,
     land=lm,
     label="mslp",
 )
--- models/DCVAE_single_PUV/validation/plot_prmsl_comparison.py
+++ models/DCVAE_single_PUV/validation/plot_prmsl_comparison.py
@@ -6,8 +6,6 @@ import sys
 import iris
 import numpy as np
 import tensorflow as tf
-import matplotlib
-import cmocean
 
 sys.path.append("%s/../../../lib/" % os.path.dirname(__file__))
 from geometry import to_analysis_grid
@@ -24,12 +22,13 @@ def plot_PRMSL(
     ax,
     t_in,
     t_out,
+    obs=None,
+    obs_c="black",
+    obs_cmap="RdBu",
     land=None,
     label=None,
-    linewidths=[1, 1],
-    d_max=1,
-    d_min=0,
-    c_space=0.1,
+    c_space=0.3,
+    linewidths=[1, 1, 1],
 ):
     if land is None:
         land = get_land_mask()
@@ -53,7 +52,7 @@ def plot_PRMSL(
                 linewidths=linewidths[0],
                 linestyles="solid",
                 alpha=1.0,
-                levels=np.arange(d_min, d_max, c_space),
+                levels=np.arange(-3, 3, c_space),
                 zorder=20,
             )
     # Encoder output
@@ -71,9 +70,22 @@ def plot_PRMSL(
                 linewidths=linewidths[1],
                 linestyles="solid",
                 alpha=1.0,
-                levels=np.arange(d_min, d_max, c_space),
+                levels=np.arange(-3, 3, c_space),
                 zorder=30,
             )
+    # Observations
+    if obs is not None:
+        obs = tf.squeeze(obs)
+        ax.scatter(
+            (obs[:, 1].numpy() / 160) * 360 - 180,
+            (obs[:, 0].numpy() / 80) * 180 - 90,
+            s=3.0 * linewidths[2],
+            c=obs_c,
+            cmap=obs_cmap,
+            marker="o",
+            alpha=1.0,
+            zorder=10,
+        )
     ax.text(
         -175,
         -85,
@@ -84,15 +96,13 @@ def plot_PRMSL(
         bbox=dict(
             facecolor=(0.8, 0.8, 0.8, 0.8), edgecolor="black", boxstyle="round", pad=0.5
         ),
-        size=4,
+        size=8,
         clip_on=True,
         zorder=40,
     )
 
 
 def plot_scatter(ax, t_in, t_out, d_max=3, d_min=-3):
-    ax.set_xlim(d_min, d_max)
-    ax.set_ylim(d_min, d_max)
     t_in = tf.squeeze(t_in)
     if tf.rank(t_in) != 2:
         raise Exception("Unsupported input data shape")
@@ -100,29 +110,14 @@ def plot_scatter(ax, t_in, t_out, d_max=3, d_min=-3):
     if tf.rank(t_out) == 2:
         t_out = tf.expand_dims(t_out, axis=0)
     t_list = tf.unstack(t_out, axis=0)
-    hb_in = None
-    hb_out = None
     for t_out in t_list:
-        if hb_in is None:
-            hb_in = t_in.numpy().flatten()
-            hb_out = t_out.numpy().flatten()
-        else:
-            hb_in = np.concatenate((hb_in, t_in.numpy().flatten()))
-            hb_out = np.concatenate((hb_out, t_out.numpy().flatten()))
-    ax.hexbin(
-        x=t_in.numpy().flatten(),
-        y=t_out.numpy().flatten(),
-        cmap=cmocean.cm.ice_r,
-        bins="log",
-        mincnt=1,
-    )
-    ax.add_line(
-        matplotlib.lines.Line2D(
-            xdata=(d_min, d_max),
-            ydata=(d_min, d_max),
-            linestyle="solid",
-            linewidth=0.5,
-            color=(0.5, 0.5, 0.5, 1),
-            zorder=100,
+        ax.scatter(
+            x=t_in.numpy().flatten(),
+            y=t_out.numpy().flatten(),
+            c="black",
+            alpha=0.25,
+            marker=".",
+            s=2,
         )
-    )
+    ax.set(ylabel="Original", xlabel="Encoded")
+    ax.grid(color="black", alpha=0.2, linestyle="-", linewidth=0.5)
